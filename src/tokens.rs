38ab2e9e4fa4593fb3b4ff3cde18cec87f3ef638
Osama Ahmad <osama199915@gmail.com>
2023-09-03 22:23:50 +0300

    Extract tokens into a separate file

---
+++ src/tokens.rs
@@ -0,0 +1,28 @@
+#[derive(Debug, PartialEq, Eq)]
+pub enum TokenType {
+    Identifier,
+    Type,
+    Keyword,
+    Equals,
+    Plus,
+    SemiColon,
+    IntegerLiteral,
+    Eof,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Token {
+    pub value: String,
+    pub token_type: TokenType,
+    pub pos: usize,
+}
+
+pub static EOF: char = '\0';
+
+// TODO update these vectors to represent the specification.
+pub static STR_ALLOWED_SYMBOLS: [char; 2] = ['_', '$'];
+
+pub static TYPES: [&str; 1] = ["int"];
+
+pub static KEYWORDS: [&str; 4] = ["if", "while", "do", "return"];
+
