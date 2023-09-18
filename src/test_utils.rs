b152a35fee8786d4643be0dde36d063914bf5c05
Osama Ahmad <osama199915@gmail.com>
2023-09-18 20:13:21 +0300

    Test util function create temp files locally to avoid permissions-related issues in CI/CD pipelines

--- src/test_utils.rs
+++ src/test_utils.rs
@@ -1,31 +1,38 @@
-use std::fmt::format;
-use std::fs::File;
+use std::fs::{File, remove_file};
 use std::io::Write;
 use std::process::Command;
 use uuid::Uuid;
-pub fn expect_return_code(source: String, expected: i32) {
-    let id = Uuid::new_v4();
-    let base_path = std::env::temp_dir();
-    let source_path = format!("{}_{}.s", base_path.display(), id);
-    let exe_path = format!("{}_{}.exe", base_path.display(), id);
+pub fn expect_return_code(source: String, expected: i32) -> std::io::Result<()> {
 
-    // FIXME: Make sure tempfiles are not deleted before being used by subsequent steps.
-    {
-        let mut executable = File::create(&exe_path).unwrap();
-        let mut source_file = File::create(&source_path).unwrap();
-        write!(source_file, "{}", source).unwrap();
-    }
-    let mut child = Command::new("gcc")
-        .arg(&source_path)
+    // Instead of using named temp files, normal files are
+    // created in the same directory to avoid permissions-
+    // related problems in the CI/CD pipelines.
+
+    let id = Uuid::new_v4();
+    let src_path = format!("./{}.s", id);
+    let exe_path = format!("./{}.exe", id);
+
+    let mut src = File::create(&src_path)?;
+    src.write_all(source.as_bytes());
+
+    // FIXME the created files may not get deleted in case
+    //  of an error in the execution of one of the commands
+    let mut output = Command::new("gcc")
+        .arg(&src_path)
         .arg("-o")
         .arg(&exe_path)
-        .spawn()
+        .output()
         .expect("Failed to compile generated code");
-    let exit_code = child.wait().unwrap();
+    let exit_code = output.status;
+    remove_file(&src_path);
     assert!(exit_code.success());
 
-    let output = Command::new(exe_path)
+    let mut output = Command::new(&exe_path)
         .output()
         .expect("Failed to execute generated executable");
-    assert_eq!(expected, output.status.code().unwrap());
+    let exit_code = output.status.code().unwrap();
+    remove_file(&exe_path);
+    assert_eq!(expected, exit_code);
+
+    Ok(())
 }
