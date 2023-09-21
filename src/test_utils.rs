448f9109018a8a9786a41a7857ae2e2bdd6efdbd
Osama Ahmad <osama199915@gmail.com>
2023-09-22 01:29:17 +0300

    expect_exit_code: panic on io errors

--- src/test_utils.rs
+++ src/test_utils.rs
@@ -6,14 +6,14 @@ pub fn expect_exit_code(source: String, expected: i32) -> std::io::Result<()> {
 
     // Instead of using named temp files, normal files are
     // created in the same directory to avoid permissions-
-    // related problems in the CI/CD pipelines.
+    // related problems in the CI pipelines.
 
     let id = Uuid::new_v4();
     let src_path = format!("./{}.s", id);
     let exe_path = format!("./{}.exe", id);
 
     let mut src = File::create(&src_path)?;
-    src.write_all(source.as_bytes());
+    src.write_all(source.as_bytes())?;
 
     // FIXME the created files may not get deleted in case
     //  of an error in the execution of one of the commands
@@ -24,19 +24,19 @@ pub fn expect_exit_code(source: String, expected: i32) -> std::io::Result<()> {
         .output()
         .expect("Failed to compile generated code");
     let exit_code = output.status;
-    remove_file(&src_path);
+    remove_file(&src_path)?;
     assert!(exit_code.success());
 
     let mut output = Command::new(&exe_path)
         .output()
         .expect("Failed to execute generated executable");
     let exit_code = output.status.code().unwrap();
-    remove_file(&exe_path);
+    remove_file(&exe_path)?;
 
-    // Some platforms return a full-precision number
-    // while others only return the least significant
-    // byte. We cut both down to their least significant
-    // byte for tests to be consistent across platforms.
+    // Some platforms return an int32 while others only
+    // return the least significant byte. We truncate
+    // both down to their least significant byte for
+    // tests to be consistent across platforms.
     assert_eq!(expected % 256, exit_code % 256);
 
     Ok(())
