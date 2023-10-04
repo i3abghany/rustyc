f633d3f2ec078c9e41438965df1ff0eae7fa94a8
Osama Ahmad <osama199915@gmail.com>
2023-10-04 09:27:59 +0300

    Remove unused code in main

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,4 @@ mod test_utils;
 mod tokens;
 
 fn main() {
-    let source = "for (i = 0; i < 10; i = i + 1;) { int x = 10; }";
-    let tokens = lexer::Lexer::new(source.to_string()).lex();
-    let ast = parser::Parser::new(tokens).parse();
-    print!("{:#?}", ast)
 }
