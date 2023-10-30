e9d08eabe53671f36fce0cd88369b83536599fd0
Osama Ahmad <osama199915@gmail.com>
2023-10-30 03:47:40 +0200

    Support for loops

--- src/syntax_analysis/ast.rs
+++ src/syntax_analysis/ast.rs
@@ -25,6 +25,6 @@ pub enum ASTNode {
     If(Token, Box<ASTNode>, Box<ASTNode>, Option<Box<ASTNode>>), // If, Condition, Body, Else
     While(Token, Box<ASTNode>, Box<ASTNode>),                    // While, Condition, Body
     DoWhile(Token, Box<ASTNode>, Token, Box<ASTNode>),           // Do, Body, While, Condition
-    For(Token, [Box<ASTNode>; 3], Box<ASTNode>), // For, [Declaration, Assignment, Condition, Update], Body
+    For(Token, [Box<ASTNode>; 3], Box<ASTNode>), // For, [Definition/Declaration, Condition, Update], Body
     Scope(Vec<ASTNode>),
 }
