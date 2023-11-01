97f345696c8d97712eadf1dbba26cbdaf97cd0af
Mahmoud Abumandour <ma.mandourr@gmail.com>
2023-10-31 18:03:33 -0700

    LLVM: Support unary operators

---
+++ src/tests/operators.c
@@ -0,0 +1,79 @@
+// Case Binary operator +
+// Returns 3
+
+int main() {
+    return 1 + 2;
+}
+
+// Case Binary operator -
+// Returns -1
+
+int main() {
+    return 1 - 2;
+}
+
+// Case Binary operator *
+// Returns 10
+
+int main() {
+    return 5 * 2;
+}
+
+// Case Binary operator *
+// Returns 4
+
+int main() {
+    return 9 / 2;
+}
+
+// Case Binary arithmetic operators combined
+// Returns 9697
+
+int main() {
+    int x = 312;
+    int y = 99;
+    int z;
+    z = 2 * x / 3 + y * y;
+    return z - x;
+}
+
+// Case Binary operator ||
+// Returns 1
+
+int main() {
+    int false = 0; int true = 123; return true || false;
+}
+
+// Case Binary operator &&
+// Returns 0
+
+int main() {
+    int false = 0; int true = 123; return true && false;
+}
+
+// Case Binary operator ^
+// Returns 123
+
+int main() {
+    int false = 0; int true = 123; return true ^ false;
+}
+
+// Case Unary operator ! 1
+// Returns 1
+
+int main() {
+    return !0;
+}
+
+// Case Unary operator ! 2
+// Returns 0
+
+int main() {
+    return !1;
+}
+
+// Case Unary +-
+// Returns 144
+int main() {
+    int x = -----++++----+------12; return x * x;
+}
